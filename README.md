435275580344138ce4ff0b755962410ff0d131e8
Giacomo Zheng <giacomo821@outlook.com>
2024-05-23 01:16:16 +0100

    Version 1

---
+++ README.md
@@ -0,0 +1,12 @@
+# live_compile_hitex
+
+update `.tex` into `.hnt` by hilatex, whenever changes happen
+
+Usage: live_compile_hitex [DIR]
+
+Arguments:
+  [DIR]  directory to watch [default: .]
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+#![feature(const_trait_impl)]
+use std::ffi::OsStr;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+use std::time::{SystemTime, Duration};
+use std::sync::mpsc;
+use std::thread;
+use std::str::from_utf8;
+
+use chrono::Local;
+use inline_colorization::{
+    color_red as WARN,
+    color_magenta as DEBUG, 
+};
+const RESET: &'static str = "\u{1b}[39m\u{1b}[49m";
+
+fn now() -> String {Local::now().format("%H:%M:%S").to_string()}
+
+const TEX_MAIN : &'static str = "main.tex";
+
+fn compile_tex(dir: &str) {
+    let output = Command::new("hilatex").arg(Path::new(dir).join(TEX_MAIN)).output().expect("failed to execute process");
+    let e = from_utf8(&output.stdout).unwrap();
+    eprintln!("{DEBUG}Debug:{RESET} Output: {}", e);
+    for error_line in e.lines()
+                        .filter(|line| line.starts_with("!")) {
+        println!("{WARN}{}{RESET}", error_line);
+    }
+}
+
+fn watch_hnt_files(dir: &str) {
+    let (tx, rx) = mpsc::channel();
+    let mut hnt_time = SystemTime::now();
+
+    let dir_entries = fs::read_dir(Path::new(dir)).unwrap();
+    for path in dir_entries.map(|e| e.unwrap().path())
+                                    .filter(|p| Some(OsStr::new("tex")) == p.extension()) {
+        let tx = tx.clone();
+        thread::spawn(move || loop {
+            let modified_time = fs::metadata(path.clone()).unwrap().modified().unwrap();
+            if modified_time > hnt_time {
+                eprintln!("{DEBUG}Debug:{RESET} tx: {:?}, {}", path, now());
+                tx.send(true).unwrap();
+                hnt_time = modified_time;
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+    }
+
+    loop {
+        let _ = rx.recv().unwrap();
+        compile_tex(dir);
+        println!("Detected modification in {:?}, {}", Path::new(dir).join(TEX_MAIN), now());
+    }
+}
+
+
+
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+/// update .tex into .hnt by hilatex, whenever changes happen 
+struct Args {
+    /// directory to watch
+    #[arg(default_value = ".")]
+    dir: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    watch_hnt_files(&args.dir);
+}
